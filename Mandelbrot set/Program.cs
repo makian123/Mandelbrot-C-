501d989d10ca9a4973d6c84c482cd73585ba1b1f
Makian Lel <hohosmarko@gmail.com>
2021-03-13 18:30:44 +0100

    Initial commit

---
+++ Mandelbrot set/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mandelbrot_set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+"# Mandelbrot-C-" 
+Calculating if a in an array belongs to the mandelbrot set
+(Possibility of ascii graphics)
