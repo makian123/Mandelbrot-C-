d8137c7e9654915f8be093253b25a9bc921c2666
Makian Lel <hohosmarko@gmail.com>
2021-03-13 19:25:01 +0100

    Calculates if the number is in mandelbrot

--- Mandelbrot set/Program.cs
+++ Mandelbrot set/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Mandelbrot_set
 {
@@ -6,7 +7,36 @@ namespace Mandelbrot_set
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double xPos = 1;
+            double yPos = 0;
+            int iterations = 1000;
+
+            double[] point = new double[2] { xPos, yPos };
+            Console.WriteLine(belongsToSet(point, iterations)); //Returns true if number is in set, false if not
+        }
+
+        static bool belongsToSet(double[] z, int iterations)
+        {
+            while(iterations > 0)
+            {
+                if(Math.Abs(z[0]) > 2 || Math.Abs(z[1]) > 2)    //Check if number goes over the limit
+                {
+                    return false;
+                }
+                z = square(z);  //Square the number
+                --iterations;
+            }
+
+            return true;
+        }
+
+        private static double[] square(double[] z)
+        {
+            double rResult, iResult;    //Real and imaginary numbers
+            rResult = z[0] * z[0] - z[1] * z[1];    //Calculates the real number
+            iResult = 2 * z[1] * z[1];  //Calculates the imaginary
+
+            return new double[2] {rResult, iResult};    //Returns the real and imaginary numbers
         }
     }
 }
